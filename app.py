b5620985233efb0b201241e2ce2c61d298423286
maxime lu <merdeth69@gmail.com>
2023-04-28 01:21:05 +0200

    oektoek

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import snowflake.connector
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from ELDINGUSRINGUS;")
+
+st.table(data=rows)
